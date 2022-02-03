66d0efc42b903230a832efe5bd8ec99328924fdf
Desmond Parker <evan.parker95@gmail.com>
2022-02-03 23:34:58 +0000

    functioning login/logout/signup options. error message popups to be added at a later date

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,13 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
   get '/about' => 'about#show'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
