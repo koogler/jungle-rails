1eb8fc1fc746ceac91ecd8d9b94c423e79bb8cff
Desmond Parker <evan.parker95@gmail.com>
2022-02-03 01:37:46 +0000

    adding inventory information to the admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,10 @@
 class Admin::DashboardController < ApplicationController
 
   http_basic_authenticate_with name: ENV['AUTHENTICATION_USERNAME'], password: ENV['AUTHENTICATION_PASSWORD']
+  
   def show
+    @product_num = Product.all.count
+    @category_num = Category.all.count
   end
 
 end
